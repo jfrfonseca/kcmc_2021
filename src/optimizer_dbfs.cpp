c50578a5d835c91590c1f91ba2a8f654519c4462
José Fonseca <jose@denox.com.br>
2022-05-08 22:21:21 -0300

    Added the flood-dinic heuristic, that floods found DINIC paths to get non-size-increasing alternatives. More variations might be needed.

--- src/optimizer_dbfs.cpp
+++ src/optimizer_dbfs.cpp
@@ -59,6 +59,142 @@ int KCMC_Instance::directed_bfs(std::unordered_set<int> &seed_sensors,
 }
 
 
+/** FLOOD-DINIC ALGORITM
+ * For each POI, finds M node-disjoint paths connecting the POI to the SINK. Then "floods" the set of POIs found paths.
+ * Flooding: Let path A connect POI P to sink S. Let A also be be a sequence of active sensors so that the first sensor
+ * i0 in the sequence covers POI P, and the last sensor ik connects to sink S. So, A = (P)i0,i1,...ik(S).
+ * The "flooded" version of path A is a set of sensors that contains, for each connected triple ix-1, ix, ix+1 in A,
+ * all sensors that connect both to ix-1 and ix+1. At the starting edge of A, ix-1 is P. At the end edge of A, ix+1 is S
+ */
+int KCMC_Instance::flood_dinic(const int k, const int m, std::unordered_set<int> &inactive_sensors, std::unordered_set<int> *result_buffer) {
+
+    // Base case
+    if (m < 1){return -1;}
+
+    // Create the level graph, loop controls and buffers
+    bool break_loop;
+    int level_graph[this->num_sensors], predecessors[this->num_sensors],
+        paths_found, path_end, a_poi, path_length, last_path_length, previous, next_i,
+        total_paths_found = 0;
+
+    // Update the level graph
+    this->level_graph(level_graph, inactive_sensors);
+
+    // Prepare the set of "used" sensors for each POI
+    std::unordered_set<int> used_sensors;
+
+    // Validate K-Coverage
+    if (this->fast_k_coverage(k, inactive_sensors, &used_sensors) != -1) {
+        throw std::runtime_error("INVALID INSTANCE! (INSUFFICIENT COVERAGE)");
+    }
+
+    // Reset the results buffer
+    result_buffer->clear();
+
+    // Add all poi-covering sensors to the result buffer
+    for (a_poi=0; a_poi < this->num_pois; a_poi++) {
+        for (const int &a_sensor : this->poi_sensor[a_poi]) {
+            result_buffer->insert(a_sensor);
+        }
+    }
+
+    // Run for each POI, returning at the first failure
+    for (a_poi=0; a_poi < this->num_pois; a_poi++) {
+        break_loop = false;  // Mark the loop for processing
+        paths_found = 0;  // Clear the number of paths found for the POI
+        last_path_length = 0; // reset the stored length of the last found path
+        used_sensors = inactive_sensors;  // Reset the set of used sensors for each POI
+
+        // While the stopping criteria was not found
+        while (not break_loop) {
+            std::fill(predecessors, predecessors+this->num_sensors, -2);  // Reset the predecessors buffer
+
+            // Find a path
+            path_end = this->find_path(a_poi, used_sensors, level_graph, predecessors);
+
+            // Reset the control buffers
+            next_i = -1;
+            path_length = 0;
+
+            // If the path ends in an invalid sensor, mark the loop to end. If we do not have enough paths, throu error
+            if (path_end == -1) {
+                break_loop = true;
+                if (paths_found < m) { throw std::runtime_error("INVALID INSTANCE! (INSUFFICIENT CONNECTIVITY)"); }
+            }
+
+            // If it is a sucessful path
+            else {
+                paths_found += 1;  // Count the newfound path
+                total_paths_found += 1;
+
+                // Unravel the path, marking each sensor in it as used and flooding it
+                while (path_end != -1) {
+                    used_sensors.insert(path_end);
+                    path_length += 1;
+
+                    // Get the previous sensor in the path
+                    previous = predecessors[path_end];
+                    if (previous == -2) { throw std::runtime_error("FORBIDDEN ADDRESS!"); }
+
+                    /* If the previous sensor is a POI and the next is a SINK
+                     * Add all active sensors that connect both to the POI and the SINK to the result buffer
+                     */
+                    if ((previous == -1) and (next_i == -1)) {
+                        for (const int &bridge: this->poi_sensor[a_poi]) {
+                            if (isin(this->sensor_sink, bridge) and (not isin(inactive_sensors, bridge))) {
+                                result_buffer->insert(bridge);
+                            }
+                        }
+                    } else {
+                        /* If the previous sensor is a POI (and the next cannot be a SINK)
+                         * Add all active sensors that cover the POI and connect to the path_end sensor to the result
+                         */
+                        if (previous == -1) {
+                            for (const int &cover: this->poi_sensor[a_poi]) {
+                                if (isin(this->sensor_sensor[cover], path_end) and (not isin(inactive_sensors, cover))) {
+                                    result_buffer->insert(cover);
+                                }
+                            }
+                        } else {
+                            /* If the previous sensor is NOT a POI and the next IS a SINK
+                             * Add all active sensors that connect to both the previous sensor and the sink
+                             */
+                            if (next_i == -1) {
+                                for (const int &conn: this->sensor_sensor[previous]) {
+                                    if (isin(this->sensor_sink, conn) and (not isin(inactive_sensors, conn))) {
+                                        result_buffer->insert(conn);
+                                    }
+                                }
+                            } else {
+                                /* If the previous sensor is NOT a POI ant the next is NOT a sink
+                                 * Add all active sensors that connect to both the previous and the next to the result
+                                 */
+                                for (const int &conn: this->sensor_sensor[previous]) {
+                                    if (isin(this->sensor_sensor[conn], next_i) and (not isin(inactive_sensors, conn))) {
+                                        result_buffer->insert(conn);
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    // Mark that the next in the path is not a sink and advance the path
+                    next_i = path_end;
+                    path_end = previous;
+                }
+
+                // If we have enough paths, but the current is no larger than the last, continue the loop. Otherwise stop
+                if (paths_found <= m) { last_path_length = path_length; }
+                if (path_length > last_path_length) { break_loop = true; }
+            }
+        }
+    }
+
+    // Success in each and every POI! Return the total of found paths
+    return total_paths_found;
+}
+
+
 /* #####################################################################################################################
  * RUNTIME
  * */
@@ -161,6 +297,17 @@ int main(int argc, char* const argv[]) {
     end = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
     printout_short(instance->key(), k, m, instance->num_sensors, "directed_bfs", duration, used_installation_spots);
+    used_installation_spots.clear();
+
+    // Process the Flood-Dinic mapping of the instance
+    start = std::chrono::high_resolution_clock::now();
+    poi = instance->flood_dinic(k, m, emptyset, &used_installation_spots);
+    end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    printout_short(instance->key(), k, m, instance->num_sensors,
+                   "f_dinic_"+std::to_string(poi),  // Add the number of paths found
+                   duration, used_installation_spots);
+    used_installation_spots.clear();
 
     return 0;
 }

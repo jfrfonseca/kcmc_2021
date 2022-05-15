affb8a1acbf6236540b2b45a804050c258bd6344
José Fonseca <jose@denox.com.br>
2022-05-15 11:03:05 -0300

    Added instance regeneration

--- src/kcmc_instance.py
+++ src/kcmc_instance.py
@@ -27,6 +27,21 @@ def get_placements(pois, sensors, sinks, area_side, random_seed, executable='/ap
     return placements
 
 
+# Get the regenerated instance from its key using the C++ interface
+def get_serialized_instance(pois, sensors, sinks, area_side, sensor_coverage_radius, sensor_communication_radius, random_seed,
+                   executable='/app/instance_evaluator'):
+    out = subprocess.Popen(
+        list(map(str, [
+            executable, 0, 0,
+            f'KCMC;{pois} {sensors} {sinks}; {area_side} {sensor_coverage_radius} {sensor_communication_radius}; {random_seed}; END'
+        ])),
+        stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+    )
+    stdout,stderr = out.communicate()
+    assert stderr is None, f'ERROR ON THE INSTANCE REGENERATOR.\nSTDOUT:{stdout}\n\nSTDERR:{stderr}'
+    return stdout.decode().strip().splitlines()[0].strip()
+
+
 class KCMC_Instance(object):
 
     color_dict = {
@@ -42,7 +57,8 @@ class KCMC_Instance(object):
 
     def __repr__(self): return f'<{self.key_str} {self.random_seed} [{len(self.virtual_sinks)}]>'
 
-    def __init__(self, instance_string:str, accept_loose_pois=False, accept_loose_sensors=False, accept_loose_sinks=False):
+    def parse_serialized_instance(self, instance_string:str):
+
         self.string = instance_string.upper().strip()
 
         assert self.string.startswith('KCMC;'), 'ALL INSTANCES MUST START WITH THE TAG <KCMC;>'
@@ -77,6 +93,7 @@ class KCMC_Instance(object):
         self.virtual_sinks_map = {}
 
         tag = None
+        is_expanded = False
         for i, token in enumerate(instance[4:-1]):
             if token in {'PI', 'II', 'IS'}:
                 tag = token
@@ -86,19 +103,35 @@ class KCMC_Instance(object):
 
             alpha, beta = map(int, token.strip().split(' '))
             if tag == 'PI':
+                is_expanded = True
                 self._add_to(self.edges, f'p{alpha}', f'i{beta}')
                 self._add_to(self.poi_sensor, alpha, beta)
                 self._add_to(self.sensor_poi, beta, alpha)
             elif tag == 'II':
+                is_expanded = True
                 assert alpha != beta,  "SELF-DIRECTED EDGES ARE NOT SUPPORTED"
                 self._add_to(self.edges, f'i{alpha}', f'i{beta}')
                 self._add_to(self.sensor_sensor, alpha, beta)
                 self._add_to(self.sensor_sensor, beta, alpha)
             elif tag == 'IS':
+                is_expanded = True
                 self._add_to(self.edges, f'i{alpha}', f's{beta}')
                 self._add_to(self.sensor_sink, alpha, beta)
                 self._add_to(self.sink_sensor, beta, alpha)
             else: raise AssertionError(f'IMPOSSIBLE TAG {tag}')
+
+        # If not expanded, regenerate the instance and re-parse
+        if not is_expanded:
+            self.parse_serialized_instance(get_serialized_instance(
+                self.num_pois, self.num_sensors, self.num_sinks,
+                self.area_side, self.sensor_coverage_radius, self.sensor_communication_radius,
+                self.random_seed
+            ))
+
+    def __init__(self, instance_string:str, accept_loose_pois=False, accept_loose_sensors=False, accept_loose_sinks=False):
+
+        # Parse the serialized instance
+        self.parse_serialized_instance(instance_string)
 
         # Reading validations
         assert max(self.poi_sensor.keys()) < self.num_pois, f'INVALID POI IDs! {[p for p in self.poi_sensor.keys() if p > self.num_pois]}'

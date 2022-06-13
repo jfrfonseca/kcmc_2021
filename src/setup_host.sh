b64f73a3719af714684f09e15769d816e3c9582b
José Fonseca <jose@denox.com.br>
2022-06-13 15:30:29 -0300

    Added more comments

--- src/setup_host.sh
+++ src/setup_host.sh
@@ -11,14 +11,21 @@ echo '/swapfile swap swap defaults 0 0' | sudo tee -a /etc/fstab
 sudo yum install -y git docker htop
 sudo pip3 install docker-compose
 
+# Clone the repo
+git clone https://github.com/jfrfonseca/kcmc_heuristic.git
+cd kcmc_heuristic
+
+# Untar the instances
+cd data
+tar -xzvf instances.tar.gz
+cd ../
+
 # Prepare the docker runtime
 sudo usermod -a -G docker ec2-user
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 
-# Clone the repo
-git clone https://github.com/jfrfonseca/kcmc_heuristic.git
-cd kcmc_heuristic
+# You might have to LOG-OUT and then LOG IN again after running the code above and before running the code below
 
 # Compile the C++ code
 docker-compose --profile compiler build
@@ -27,9 +34,15 @@ docker-compose --profile compiler up
 # Prepare the optimizer
 docker-compose --profile optimizer build
 
-# Untar the instances
-cd data
-tar -xzvf instances.tar.gz
-cd ../
-
 # Now you must prepare your gurobi licences file and your .env environment file by hand
+#
+# Example for the .env file:
+# rm .env && nano .env
+# And then fill the file
+#
+# Example for the license file
+# nano data/licence.lic
+# And then fill the file
+
+# And then you can run the optimizer stack, with:
+# nohup docker-compose --profile optimizer up &. log.out &

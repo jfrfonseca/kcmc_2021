db7dd152f5d2f73db456fd5b46e092b642f2d14f
José Fonseca <jose@denox.com.br>
2022-06-13 14:45:53 -0300

    Master repo cleanup

--- src/setup_host.sh
+++ src/setup_host.sh
@@ -6,3 +6,12 @@ sudo pip3 install docker-compose
 
 # Clone the repo
 git clone https://github.com/jfrfonseca/kcmc_heuristic.git
+cd kcmc_heuristic
+
+# Compile the C++ code
+sudo docker-compose --profile compiler build
+sudo docker-compose --profile compiler up
+
+# Prepare the optimizer
+sudo docker-compose --profile optimizer build
+sudo docker-compose --profile optimizer up

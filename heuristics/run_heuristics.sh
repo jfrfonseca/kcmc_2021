e045172fb3c7ced5dd632ce5e840f92af4fb0eea
José Fonseca <jose@denox.com.br>
2023-01-22 18:11:15 -0300

    Improved the runtime of the optimizer

--- heuristics/run_heuristics.sh
+++ heuristics/run_heuristics.sh
@@ -5,6 +5,7 @@ if [ ! -f /data/instances.csv ]
 then
 
   # Process the data parallely
+  echo "GENERATING INSTANCES"
   parallel -a /app/heuristics/instance_classes.csv --colsep ' ' --files /app/instances_generator
   # In case of frequent isomorphism, try to run twice to get spare instances
   #sleep 2
@@ -13,7 +14,14 @@ then
   # Collect the instances and give appropriate permissions
   cat /tmp/*.par > /data/instances.csv
   chmod a+rw /data/instances.csv
+
+  # Test isomorphism. Hopefully we will have enough instances anyway
+  echo "TESTING ISOMORPHISM IN GENERATED INSTANCES"
+  cat /data/instances.csv | python -u /app/integer_linear_programs/kcmc_instance.py
 fi
 
-# Test isomorphism. Hopefully we will have enough instances anyway
-cat /data/instances.csv | python -u /app/integer_linear_programs/kcmc_instance.py
+# Optimize instances parallely using all heuristics, then collect the results
+echo "OPTIMIZING INSTANCES HEURISTICALLY"
+parallel -a /data/instances.csv --colsep '\t' --files /app/optimizer
+cat /tmp/*.par > /data/optimized_instances.csv
+chmod a+rw /data/optimized_instances.csv

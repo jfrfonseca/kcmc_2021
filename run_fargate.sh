8395377694e254c9fd5299119e28c451449495f1
José Fonseca <jose@denox.com.br>
2023-01-31 22:28:19 -0300

    Set up FARGATE runtime

--- run_fargate.sh
+++ run_fargate.sh
@@ -8,18 +8,30 @@ set -e
 source .env
 COUNT=1
 
-
 # Prepare the payload for the OPTIMIZER GUROBI
-if [ $1 = "gurobi" ]; then
+if [ ${1} = "gurobi" ]; then
 echo "Using GUROBI"
+
+# Prepare the command. The hard part is to make it split right
+PYARGS=${@}
+COMMAND=$(python << EOF0
+import shlex, json, sys
+args = "${PYARGS}"
+pref, suf = args.split('KCMC;')
+key, suf = suf.split(';END')
+key = 'KCMC;'+key.replace(' ', '_')+';END'
+args = pref+key+suf
+args = shlex.split(args)[2:]
+print(json.dumps(["/app/run_gurobi.sh"]+args))
+EOF0
+)
+
+# Prepare the container overrides
 OVERRIDES=$(jq --compact-output << EOF
 {
   "containerOverrides": [{
     "name": "worker",
-    "command": [
-      "/app/run_gurobi.sh", "KCMC;100_500_1;300_50_100;157982033;END", "3", "2",
-      "multi-flow"
-    ],
+    "command": ${COMMAND},
     "cpu": 1024,
     "memory": 8192,
     "environment": [
@@ -73,4 +85,5 @@ aws ecs run-task \
   --network-configuration ${NETWORK_CONF} \
   --launch-type FARGATE \
   --region ${AWS_REGION} \
-  --overrides ${OVERRIDES}
+  --overrides ${OVERRIDES} \
+  --query "tasks[0].taskArn"
